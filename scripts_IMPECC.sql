c05af5f97dc4119ffc30ae3c1f3e47877e28f2f9
santtoseduarda <eduarda.s2007@aluno.ifsc.edu.br>
2024-05-07 11:22:03 -0300

    Alterçoes no codigo

--- scripts_IMPECC.sql
+++ scripts_IMPECC.sql
@@ -5,9 +5,9 @@ CREATE DATABASE IF NOT EXISTS impecc;
 USE impecc;
 
 -- -----------------------------------------------------
--- Table Produtos
+-- Table mydb.Produtos
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Produtos (
+CREATE TABLE IF NOT EXISTS mydb.Produtos (
     id_Produtos INT NOT NULL,
     Nome_Cliente VARCHAR(45) NOT NULL,
     Modelo VARCHAR(45) NOT NULL,
@@ -20,11 +20,12 @@ CREATE TABLE IF NOT EXISTS Produtos (
 );
 
 -- -----------------------------------------------------
--- Table Clientes
+-- Table mydb.Clientes
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Clientes (
+CREATE TABLE IF NOT EXISTS mydb.Clientes (
     id_Clientes INT NOT NULL,
     Nome_Cliente VARCHAR(45) NOT NULL,
+    Sobrenome VARCHAR(45) NOT NULL,
     Data_Nasc DATE NOT NULL,
     Cpf VARCHAR(45) NOT NULL,
     Telefone VARCHAR(45) NOT NULL,
@@ -33,9 +34,9 @@ CREATE TABLE IF NOT EXISTS Clientes (
 );
 
 -- -----------------------------------------------------
--- Table Funcionarios
+-- Table mydb.Funcionarios
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Funcionarios (
+CREATE TABLE IF NOT EXISTS mydb.Funcionarios (
     id_Funcionarios INT NOT NULL,
     Nome_Funcionario VARCHAR(45) NOT NULL,
     Cargo VARCHAR(45) NOT NULL,
@@ -45,28 +46,28 @@ CREATE TABLE IF NOT EXISTS Funcionarios (
 );
 
 -- -----------------------------------------------------
--- Table Vendas
+-- Table mydb.Vendas
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Vendas (
+CREATE TABLE IF NOT EXISTS mydb.Vendas (
     id_Vendas INT NOT NULL,
     Total DECIMAL NOT NULL,
     Mtd_Pagamento VARCHAR(45) NOT NULL,
     id_Clientes INT NOT NULL,
     id_Funcionarios INT NOT NULL,
     PRIMARY KEY (id_Vendas, id_Clientes, id_Funcionarios),
-    FOREIGN KEY (id_Clientes) REFERENCES Clientes (id_Clientes),
-    FOREIGN KEY (id_Funcionarios) REFERENCES Funcionarios (id_Funcionarios)
+    FOREIGN KEY (id_Clientes) REFERENCES mydb.Clientes (id_Clientes),
+    FOREIGN KEY (id_Funcionarios) REFERENCES mydb.Funcionarios (id_Funcionarios)
 );
 
 -- -----------------------------------------------------
--- Table Historico_de_Vendas
+-- Table mydb.Historico_de_Vendas
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Historico_de_Vendas (
+CREATE TABLE IF NOT EXISTS mydb.Historico_de_Vendas (
     id_Historico_de_Vendas INT NOT NULL,
     Qtd_Venda INT NOT NULL,
     id_Vendas INT NOT NULL,
     id_Produtos INT NOT NULL,
     PRIMARY KEY (id_Historico_de_Vendas, id_Vendas, id_Produtos),
-    FOREIGN KEY (id_Vendas) REFERENCES Vendas (id_Vendas),
-    FOREIGN KEY (id_Produtos) REFERENCES Produtos (id_Produtos)
+    FOREIGN KEY (id_Vendas) REFERENCES mydb.Vendas (id_Vendas),
+    FOREIGN KEY (id_Produtos) REFERENCES mydb.Produtos (id_Produtos)
 );

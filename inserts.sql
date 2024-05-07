5a436879ef28fcbe9d18fd95e5a2e64a19fcea0f
Camili Starosky <camili.naara@gmail.com>
2024-05-07 11:11:55 -0300

    insert Produto

--- inserts.sql
+++ inserts.sql
@@ -1,2 +1,4 @@
 INSERT INTO Funcionarios (id_Funcionarios, Nome_Funcionario, Cargo, Email, Senha) VALUES
 (?, ?, ?, ?, ?);
+
+INSERT INTO Produtos (id_Produtos, Nome_Produto, Modelo, Tamanho , Genero, Preco, Fornecedor, Qntd_Estoque) VALUES (?, ?, ?, ?, ?, ?, ?, ?);

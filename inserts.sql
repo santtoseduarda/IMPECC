e16f15d21e58ec12bc4225a904426cdbb239b542
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-05-07 11:18:21 -0300

    insert venda

--- inserts.sql
+++ inserts.sql
@@ -2,3 +2,6 @@ INSERT INTO Funcionarios (id_Funcionarios, Nome_Funcionario, Cargo, Email, Senha
 (?, ?, ?, ?, ?);
 
 INSERT INTO Produtos (id_Produtos, Nome_Produto, Modelo, Tamanho , Genero, Preco, Fornecedor, Qntd_Estoque) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+
+INSERT INTO Vendas (id_Vendas, Total, Mtd_Pagamento, id_Clientes, id_Funcionarios)
+VALUES (?, ?, ?, ?, ?);

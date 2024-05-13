6af81e081bbd14ceee06c229cd720ca13c029b44
santtoseduarda <eduarda.s2007@aluno.ifsc.edu.br>
2024-05-13 09:03:41 -0300

    updates Funcionario

--- Updates.sql
+++ Updates.sql
@@ -1,9 +1,5 @@
 UPDATE Produtos SET nome_Produto = ? WHERE id_Produtos = ?;
-<<<<<<< Updated upstream
 UPDATE Clientes SET Telefone = ? WHERE id_Clientes = ?;
-=======
-
 UPDATE Vendas SET Mtd_Pagamento = ? WHERE id_Vendas = ?;
-
 UPDATE Historico_de_Vendas SET Qtd_Venda = ? WHERE id_Historico_de_Vendas = ?;
->>>>>>> Stashed changes
+UPDATE Funcionario SET Cargo = ? WHERE id_Funcionario = ?;

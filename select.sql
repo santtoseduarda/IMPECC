45d651b6a6b2245052285bbf765afabef50658fb
isabellekc24 <isabelle.kc24@aluno.ifsc.edu.br>
2024-05-13 10:51:18 -0300

    SELECT COM JOIN

--- select.sql
+++ select.sql
@@ -1,6 +1,5 @@
+--SELECT (*)
 SELECT * FROM Produtos;
-
-SELECT * FROM Funcionarios WHERE Email = ? AND Senha = ? ;
 
 SELECT * FROM Vendas;
 
@@ -10,6 +9,7 @@ SELECT * FROM Clientes;
 
 SELECT * FROM Funcionario;
 
+<<<<<<< Updated upstream
 SELECT *
 FROM Vendas AS v
 INNER JOIN Clientes AS c
@@ -22,3 +22,19 @@ FROM Vendas AS v
 INNER JOIN Funcionarios AS f
 ON
 f.id_Funcionario = v.id_Funcionario;
+=======
+--WHERE
+SELECT * FROM Funcionarios WHERE Email = ? AND Senha = ? ;
+
+--SELECT COM JOIN
+SELECT * 
+FROM Historico_de_Vendas AS hv 
+
+INNER JOIN Vendas AS v ON hv.id_Vendas = v.id_Vendas
+
+INNER JOIN Produtos AS p ON hv.id_Produtos = p.id_Produtos
+
+INNER JOIN Clientes AS c ON v.id_Clientes = c.id_Clientes
+
+INNER JOIN Funcionarios AS f ON v.id_Funcionarios = f.id_Funcionarios;
+>>>>>>> Stashed changes

df47e61b82a1ed0665bbdc5345b0eba6024b3fe5
isabellekc24 <isabelle.kc24@aluno.ifsc.edu.br>
2024-05-13 08:07:26 -0300

    Inserts da tabela Produtos

--- inserts.sql
+++ inserts.sql
@@ -7,3 +7,7 @@ INSERT INTO Vendas (id_Vendas, Total, Mtd_Pagamento, id_Clientes, id_Funcionario
 VALUES (?, ?, ?, ?, ?);
 
 INSERT INTO Clientes (id_Clientes, Nome_Cliente, Sobrenome, Data_Nasc, Cpf , Telefone, Email) VALUES (?, ?, ?, ?, ?, ?, ?);
+
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Camisa Preta', 'Cropped', 'PP', 'Feminino', '99.99', 'Nike', 30);
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Casaco', 'Ziper', 'M', 'Masculino', '135.90', 'Adidas', 25);
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Top', 'Aberto', 'G', 'Feminino', '179.20', 'Nike', 13);

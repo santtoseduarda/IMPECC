f52ebac920b5fa7422856fec0f98a79c55642d14
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-05-13 08:37:50 -0300

    Insert da tabela Clientes

--- inserts.sql
+++ inserts.sql
@@ -4,19 +4,10 @@ INSERT INTO Funcionarios (Nome_Funcionario, Cargo, Email, Senha) VALUES
 
 -- produtos
 INSERT INTO Produtos (Nome_Produto, Modelo, Tamanho , Genero, Preco, Fornecedor, Qntd_Estoque) VALUES (?, ?, ?, ?, ?, ?, ?);
-insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Camisa Preta', 'Cropped', 'PP', 'Feminino', '99.99', 'Nike', 30);
-insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Casaco', 'Ziper', 'M', 'Masculino', '135.90', 'Adidas', 25);
-insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Top', 'Aberto', 'G', 'Feminino', '179.20', 'Nike', 13);
 
 -- venda
 INSERT INTO Vendas (Total, Mtd_Pagamento)
 VALUES (?, ?);
-INSERT INTO Vendas (Total, Mtd_Pagamento)
-VALUES ('240.95', 'dinheiro');
-INSERT INTO Vendas (Total, Mtd_Pagamento)
-VALUES ('154.96', 'cartão crédito');
-INSERT INTO Vendas (Total, Mtd_Pagamento)
-VALUES ('98.99', 'cartão dépito');
 
 -- cliente
 INSERT INTO Clientes (Nome_Cliente, Sobrenome, Data_Nasc, Cpf , Telefone, Email) VALUES (?, ?, ?, ?, ?, ?, ?);
@@ -24,6 +15,22 @@ INSERT INTO Clientes (Nome_Cliente, Sobrenome, Data_Nasc, Cpf , Telefone, Email)
 -- historico venda
 INSERT INTO Historico_de_Vendas (Qtd_Venda)
 VALUES (?);
+
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Camisa Preta', 'Cropped', 'PP', 'Feminino', 99.99, 'Nike', 30);
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Casaco', 'Ziper', 'M', 'Masculino', 135.90, 'Adidas', 25);
+insert into Produtos (Nome_Produto, Modelo, Tamanho, Genero, Preco, Fornecedor, Qntd_Estoque ) values ('Top', 'Aberto', 'G', 'Feminino', 179.20, 'Nike', 13);
+
+INSERT INTO Vendas (Total, Mtd_Pagamento)
+VALUES (240.95, 'dinheiro');
+INSERT INTO Vendas (Total, Mtd_Pagamento)
+VALUES (154.96, 'cartão crédito');
+INSERT INTO Vendas (Total, Mtd_Pagamento)
+VALUES (98.99, 'cartão dépito');
+
+insert into Clientes (Nome_Cliente, Sobrenome, Data_Nasc, Cpf, Telefone, Email) values ('Oliver', 'Abate', '2003-03-25', '664.750.083-48', '(47)99457-6588', 'oabate0@nih.gov');
+insert into Clientes (Nome_Cliente, Sobrenome, Data_Nasc, Cpf, Telefone, Email) values ('Shirl', 'Ellinor', '1996-12-6', '391.736.884-56', '(55)99222-6764', 'sellinor1@shutterfly.com');
+insert into Clientes (Nome_Cliente, Sobrenome, Data_Nasc, Cpf, Telefone, Email) values ('Simonne', 'Keddle', '2001-02-27', '852.984.540-23', '(35)99030-4782', 'skeddle2@army.mil');
+
 INSERT INTO Historico_de_Vendas (Qtd_Venda)
 VALUES (3);
 INSERT INTO Historico_de_Vendas (Qtd_Venda)

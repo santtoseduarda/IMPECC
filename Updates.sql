8327a66c7fb45dcc1a938e0f2df64d7a1f03994b
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-05-13 10:50:03 -0300

    Auto stash before merge of "main" and "origin/main"

--- Updates.sql
+++ Updates.sql
@@ -1,5 +1,14 @@
 UPDATE Produtos SET Nome_Produto = ? WHERE id_Produtos = ?;
 UPDATE Clientes SET Telefone = ? WHERE id_Clientes = ?;
+=======
+
+UPDATE Clientes SET Telefone = ? WHERE id_Clientes = ?;
+
+>>>>>>> Stashed changes
 UPDATE Vendas SET Mtd_Pagamento = ? WHERE id_Vendas = ?;
 UPDATE Historico_de_Vendas SET Qtd_Venda = ? WHERE id_Historico_de_Vendas = ?;
+<<<<<<< Updated upstream
 UPDATE Funcionarios SET Cargo = ? WHERE id_Funcionarios = ?;
+=======
+
+>>>>>>> Stashed changes

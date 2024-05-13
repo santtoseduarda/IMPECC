d47d8f0b59283aa4dbbfe650528343c047b98b74
Camili <camili.ns@aluno.ifsc.edu.br>
2024-05-13 09:57:59 -0300

    select join

--- deletes.sql
+++ deletes.sql
@@ -2,14 +2,14 @@ DELETE FROM Produtos WHERE id_Produtos = ?;
 <<<<<<< Updated upstream
 
 <<<<<<< Updated upstream
-DELETE FROM Funcionario WHERE id_Funcionario = ?;
+DELETE FROM Funcionarios WHERE id_Funcionarios = ?;
 =======
 DELETE FROM Vendas WHERE id_Vendas = ?;
 
 DELETE FROM Historico_de_Vendas WHERE id_Historico_de_Vendas = ?;
 >>>>>>> Stashed changes
 =======
-DELETE FROM Funcionario WHERE id_Funcionario = ?;
+DELETE FROM Funcionarios WHERE id_Funcionarios = ?;
 DELETE FROM Clientes WHERE id_Clientes = ?;
 
 >>>>>>> Stashed changes
--- select.sql
+++ select.sql
@@ -1,6 +1,6 @@
 SELECT * FROM Produtos;
 
-SELECT * FROM Funcionario WHERE Email = ? AND Senha = ? ;
+SELECT * FROM Funcionarios WHERE Email = ? AND Senha = ? ;
 
 SELECT * FROM Vendas;
 
@@ -9,3 +9,16 @@ SELECT * FROM Historico_de_Vendas;
 SELECT * FROM Clientes;
 
 SELECT * FROM Funcionario;
+
+SELECT *
+FROM Vendas AS v
+INNER JOIN Clientes AS c
+INNER JOIN Funcionarios
+ON
+c.id_Clientes = v.id_Clientes;
+
+SELECT *
+FROM Vendas AS v
+INNER JOIN Funcionarios AS f
+ON
+f.id_Funcionario = v.id_Funcionario;

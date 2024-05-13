cd9cc354b51b9b98dce3d69547b2fd9c5493224c
Camili <camili.ns@aluno.ifsc.edu.br>
2024-05-13 08:50:58 -0300

    delete venda e historico

--- deletes.sql
+++ deletes.sql
@@ -1,3 +1,9 @@
 DELETE FROM Produtos WHERE id_Produto = ?;
 
+<<<<<<< Updated upstream
 DELETE FROM Funcionario WHERE id_Funcionario = ?;
+=======
+DELETE FROM Vendas WHERE id_Vendas = ?;
+
+DELETE FROM Historico_de_Vendas WHERE id_Historico_de_Vendas = ?;
+>>>>>>> Stashed changes

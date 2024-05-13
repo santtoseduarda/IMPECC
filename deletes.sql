656a7025cae3efca6fd2f9297b964708b4249289
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-05-13 08:52:32 -0300

    delete clientes

--- deletes.sql
+++ deletes.sql
@@ -1,4 +1,5 @@
 DELETE FROM Produtos WHERE id_Produto = ?;
+<<<<<<< Updated upstream
 
 <<<<<<< Updated upstream
 DELETE FROM Funcionario WHERE id_Funcionario = ?;
@@ -6,4 +7,9 @@ DELETE FROM Funcionario WHERE id_Funcionario = ?;
 DELETE FROM Vendas WHERE id_Vendas = ?;
 
 DELETE FROM Historico_de_Vendas WHERE id_Historico_de_Vendas = ?;
+>>>>>>> Stashed changes
+=======
+DELETE FROM Funcionario WHERE id_Funcionario = ?;
+DELETE FROM Clientes WHERE id_Clientes = ?;
+
 >>>>>>> Stashed changes

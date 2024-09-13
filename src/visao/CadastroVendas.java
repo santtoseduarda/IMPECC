fe36bc8f3576b5cf74fd6d0996b61683f6751ee1
paulomeneghelli <meneghellipaulo9@gmail.com>
2024-09-12 21:02:05 -0300

    Alteração estrutural da tela de cadastro de vendas

--- src/visao/CadastroVendas.java
+++ src/visao/CadastroVendas.java
@@ -5,6 +5,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import net.miginfocom.swing.MigLayout;
 
 public class CadastroVendas extends JFrame {
 
@@ -12,7 +13,8 @@ public class CadastroVendas extends JFrame {
     private JTextField campoCPF;
     private JLabel labelTotal;
 
-    public CadastroVendas() {
+    @SuppressWarnings("removal")
+	public CadastroVendas() {
         setTitle("Cadastro de Vendas");
         setSize(450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,10 +39,11 @@ public class CadastroVendas extends JFrame {
         ));
 
         // Criando o painel principal
-        JPanel painelPrincipal = new JPanel(new BorderLayout());
-        painelPrincipal.add(campoCPF, BorderLayout.NORTH);
-        painelPrincipal.add(new JScrollPane(tabelaProdutos), BorderLayout.EAST);
-        painelPrincipal.add(labelTotal, BorderLayout.SOUTH);
+        JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setLayout(new MigLayout("", "[452px]", "[20px][427px][14px]"));
+        painelPrincipal.add(campoCPF, "cell 0 0,growx,aligny top");
+        painelPrincipal.add(new JScrollPane(tabelaProdutos), "cell 0 1,alignx left,aligny top");
+        painelPrincipal.add(labelTotal, "cell 0 2,growx,aligny top");
 
         // Adicionando o painel ao frame
         getContentPane().add(painelPrincipal);

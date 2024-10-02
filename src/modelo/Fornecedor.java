94cbbf996cc97423a8aca0955f3bda214eebb284
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-10-02 11:45:42 -0300

    telalistagefornecedor

--- src/modelo/Fornecedor.java
+++ src/modelo/Fornecedor.java
@@ -1,16 +1,44 @@
 package modelo;
 
 public class Fornecedor extends Cliente{
-
-	private Long cnpj;
-
-	public Long getCnpj() {
-		return cnpj;
+	
+	private int ID;
+	private String nome_Fornecedor;
+	private String CNPJ;
+	private String telefone_Fornecedor;
+	private String email_Fornecedor;
+	
+	public int getID() {
+		return ID;
 	}
-
-	public void setCnpj(Long cnpj) {
-		this.cnpj = cnpj;
+	public void setID(int iD) {
+		ID = iD;
 	}
-
+	public String getNome_Fornecedor() {
+		return nome_Fornecedor;
+	}
+	public void setNome_Fornecedor(String nome_Fornecedor) {
+		this.nome_Fornecedor = nome_Fornecedor;
+	}
+	public String getCNPJ() {
+		return CNPJ;
+	}
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+	public String getTelefone_Fornecedor() {
+		return telefone_Fornecedor;
+	}
+	public void setTelefone_Fornecedor(String telefone_Fornecedor) {
+		this.telefone_Fornecedor = telefone_Fornecedor;
+	}
+	public String getEmail_Fornecedor() {
+		return email_Fornecedor;
+	}
+	public void setEmail_Fornecedor(String email_Fornecedor) {
+		this.email_Fornecedor = email_Fornecedor;
+	}
+	
+	
 	
 }

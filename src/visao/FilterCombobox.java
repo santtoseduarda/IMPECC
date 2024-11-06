953ea1631b808b02f1423fb96ee62df11b663044
paulo <meneghellipaulo9@gmail.com>
2024-11-06 11:25:34 -0300

    finalizar listagemVenda

---
+++ src/visao/FilterCombobox.java
@@ -0,0 +1,87 @@
+package visao;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class FilterCombobox extends JComboBox {
+    private List<String> array;
+
+    /*public FilterCombobox(List<String> array) { */
+    FilterCombobox(VendaController c ) { 
+        super(c.pesquisarr().toArray());
+        this.array = c.pesquisar();
+        this.setEditable(true);
+        final JTextField textfield = (JTextField) this.getEditor().getEditorComponent();
+        textfield.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent ke) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        comboFilter(textfield.getText());
+                    }
+                });
+            }
+        });
+
+    }
+
+    public void comboFilter(String enteredText) {
+        List<String> filterArray= new ArrayList<String>();
+        filterArray = controller.pesquisar(enteredText);
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).toLowerCase().contains(enteredText.toLowerCase())) {
+                filterArray.add(array.get(i));
+            }
+        }
+        if (filterArray.size() > 0) {
+            this.setModel(new DefaultComboBoxModel(filterArray.toArray()));
+            this.setSelectedItem(enteredText);
+            this.showPopup();
+        }
+        else {
+            this.hidePopup();
+        }
+    }
+
+    
+    /* Testing Codes */
+    /*
+    public static List<String> populateArray() {
+        List<String> test = new ArrayList<String>();
+        test.add("");
+        test.add("Mountain Flight");
+        test.add("Mount Climbing");
+        test.add("Trekking");
+        test.add("Rafting");
+        test.add("Jungle Safari");
+        test.add("Bungie Jumping");
+        test.add("Para Gliding");
+        test.add("isabele");
+
+        return test;
+    }
+    */
+
+    public static void makeUI() {
+        JFrame frame = new JFrame("Adventure in Nepal - Combo Filter Test");
+        FilterCombobox acb = new FilterCombobox(populateArray());
+        frame.getContentPane().add(acb);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        makeUI();
+    }
+}

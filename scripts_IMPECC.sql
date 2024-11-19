291a616a89569bfefbc44840c169875a91618d7f
isabellekc24 <isabelle.kc24@aluno.ifsc.edu.br>
2024-11-19 15:33:34 -0300

    testando tabelas

--- scripts_IMPECC.sql
+++ scripts_IMPECC.sql
@@ -3,9 +3,10 @@ DROP DATABASE  IF EXISTS impecc;
 CREATE SCHEMA IF NOT EXISTS impecc;
 USE impecc ;
 
-DROP TABLE IF EXISTS `clientes`;
-
-CREATE TABLE `clientes` (
+-- -----------------------------------------------------
+-- Table impecc.`Clientes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Clientes(
   `id_Cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nome_Cliente` varchar(45) NOT NULL,
   `data_Nasc` varchar(45) NOT NULL,
@@ -15,10 +16,10 @@ CREATE TABLE `clientes` (
   PRIMARY KEY (`id_Cliente`)
 );
 
-
-DROP TABLE IF EXISTS `fornecedor`;
-
-CREATE TABLE `fornecedor` (
+-- -----------------------------------------------------
+-- Table impecc.Fornecedor
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Fornecedores(
   `id_Fornecedor` int(11) NOT NULL AUTO_INCREMENT,
   `nome_Fornecedor` varchar(45) NOT NULL,
   `cnpj` varchar(45) NOT NULL,
@@ -27,9 +28,11 @@ CREATE TABLE `fornecedor` (
   PRIMARY KEY (`id_Fornecedor`)
 ) ;
 
-DROP TABLE IF EXISTS `funcionarios`;
 
-CREATE TABLE `funcionarios` (
+-- -----------------------------------------------------
+-- Table impecc.`Funcionarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Funcionarios(
   `id_Funcionario` int(11) NOT NULL AUTO_INCREMENT,
   `nome_Funcionario` varchar(45) NOT NULL,
   `email_Funcionario` varchar(45) NOT NULL,
@@ -39,10 +42,10 @@ CREATE TABLE `funcionarios` (
   `cpf` varchar(45) NOT NULL,
   PRIMARY KEY (`id_Funcionario`)
 ) ;
-
-DROP TABLE IF EXISTS `produtos`;
-
-CREATE TABLE `produtos` (
+-- -----------------------------------------------------
+-- Table impecc.Produtos
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Produtos(
   `id_Produto` int(11) NOT NULL AUTO_INCREMENT,
   `nome_Produto` varchar(45) NOT NULL,
   `tamanho` varchar(45) NOT NULL,
@@ -53,9 +56,11 @@ CREATE TABLE `produtos` (
   PRIMARY KEY (`id_Produto`)
 ) ;
 
-DROP TABLE IF EXISTS `venda_produtos`;
 
-CREATE TABLE `venda_produtos` (
+-- -----------------------------------------------------
+-- Table impecc.Venda_Produtos
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Venda_produtos(
   `id_Venda` int(11) NOT NULL,
   `id_Produto` int(11) NOT NULL,
   `qntd` int(11) NOT NULL,
@@ -66,9 +71,10 @@ CREATE TABLE `venda_produtos` (
   CONSTRAINT `id_Vendafk` FOREIGN KEY (`id_Venda`) REFERENCES `vendas` (`id_Venda`)
 ) ;
 
-DROP TABLE IF EXISTS `vendas`;
-
-CREATE TABLE `vendas` (
+-- -----------------------------------------------------
+-- Table impecc.Vendas
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS impecc.Vendas(
   `id_Venda` int(11) NOT NULL AUTO_INCREMENT,
   `Total` decimal(65,2) NOT NULL,
   `Mtd_Pagamento` varchar(45) NOT NULL,

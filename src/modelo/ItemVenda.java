14df831e4f633f2f0fc508736ba89c25074d9855
isabellekc24 <isabelle.kc24@aluno.ifsc.edu.br>
2024-11-19 12:00:40 -0300

    fazendo DAO

---
+++ src/modelo/ItemVenda.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class ItemVenda {
+
+	private Produto produto;
+	private float preco;
+	private int qntd;
+	
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public int getQntd() {
+		return qntd;
+	}
+	public void setQntd(int qntd) {
+		this.qntd = qntd;
+	}
+	
+	
+}
